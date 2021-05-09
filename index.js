36bd6c15ed15b0861888710dfdcff513caaf1645
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-05-09 18:07:28 -0400

    import getAllVillains function to index js file

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const villains = require('./villains')
 const express = require('express')
 const bodyParser = require('body-parser')
+const { getAllVillains } = require('./controllers/villains')
 
 const app = express()
 const PORT = 1337
 
-app.get('/villains', (req, res) => {
-  res.send('test')
-})
+app.get('/villains', getAllVillains)
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`) // eslint-disable-line no-console
