2220474814e1750c036d318748a6ca4ac09a50c2
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-05-09 20:48:58 -0400

    add to addNewVillain function so that it adds a new object to the database

--- controllers/villains.js
+++ controllers/villains.js
@@ -15,8 +15,16 @@ const getVillainsBySlug = async (req, res) => {
     ? res.send(result) : res.sendStatus(404)
 }
 
-const addNewVillain = () => {
+const addNewVillain = async (req, res) => {
+  const { name, movie, slug } = req.body
 
+  if (!name || !movie || !slug) {
+    return res.send('All fields are required')
+  }
+
+  const newVillain = await models.villains.create({ name, movie, slug })
+
+  return res.send(newVillain)
 }
 
 module.exports = { getAllVillains, getVillainsBySlug, addNewVillain }
