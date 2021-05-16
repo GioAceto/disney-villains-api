b41c3035b361b77c2d7bf9d7000dfd7cc1da62f2
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-05-16 17:22:31 -0400

    add try catch to getAllTeams function and changed status 500 message

--- test/controller/villains.test.js
+++ test/controller/villains.test.js
@@ -10,15 +10,46 @@ chai.use(sinonChai)
 const { expect } = chai
 
 describe('Controllers - Villains', () => {
+  let stubbedFindOne
+
+  before(() => {
+    stubbedFindOne = sinon.stub(models.villains, 'findOne')
+  })
+
+  afterEach(() => {
+    stubbedFindOne.resetBehavior()
+  })
+
   describe('getAllVillains', () => {
     it('retrieves a list of villains from the database and calls response.send() with the list', async () => {
       const stubbedFindAll = sinon.stub(models.villains, 'findAll').returns(villainsList)
       const stubbedSend = sinon.stub()
       const response = { send: stubbedSend }
+
       await getAllVillains({}, response)
 
       expect(stubbedFindAll).to.have.callCount(1)
       expect(stubbedSend).to.have.been.calledWith(villainsList)
+    })
+  })
+
+  describe('Get Villains by Slug', () => {
+    it('get a single villain from the DB with the provided ID', async () => {
+      stubbedFindOne.returns(singleVillain)
+      const req = { params: { name: 'Captain Hook', movie: 'Peter Pan', slug: 'captain-hook' } }
+      const stubbedSend = sinon.stub()
+      const res = { send: stubbedSend }
+
+      await getVillainsBySlug(req, res)
+
+      expect(stubbedFindOne).to.have.been.calledWith({
+        where: {
+          name: 'Captain Hook',
+          movie: 'Peter Pan',
+          slug: 'captain-hook'
+        }
+      })
+      expect(stubbedSend).to.have.been.calledWith(singleVillain)
     })
   })
 })
